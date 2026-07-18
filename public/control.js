f945c43378f00f71dcc21a402bebf4509276b6fd
Xorishi <140198006+XorishiTtv@users.noreply.github.com>
2026-07-18 18:21:16 -0400

    Load speaking source adapter before detection core

--- public/control.js
+++ public/control.js
@@ -12,7 +12,8 @@
     });
   }
 
-  loadScript('control-core.js')
+  loadScript('speaking-source-capture.js')
+    .then(() => loadScript('control-core.js'))
     .then(() => loadScript('audio-devices-control.js'))
     .catch(error => console.error('[audio-devices] Bootstrap failed:', error));
 })();
