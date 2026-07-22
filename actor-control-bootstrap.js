da1e4d8f34e72046fbba543b31964a0352bea9c4
Xorishi <140198006+XorishiTtv@users.noreply.github.com>
2026-07-22 05:28:04 -0400

    Avoid global HTML response wrapping for actor controls

--- actor-control-bootstrap.js
+++ actor-control-bootstrap.js
@@ -1,32 +1,50 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 
+const APP_DIR = process.pkg ? path.dirname(process.execPath) : __dirname;
+const CONTROL_PATH = path.join(APP_DIR, 'public', 'index.html');
 const MARKER = 'data-as-adventurer-actor-control';
-const SCRIPT = `<script src="/actor-control-panel.js" ${MARKER}></script>`;
+const LAN_CONTROL_BOOTSTRAP = [
+  '<script src="/machine-client.js" data-as-adventurer-lan-bootstrap></script>',
+  '<script src="/machine-media-url-compat.js"></script>',
+  '<script src="/machine-control-emote-sync.js"></script>',
+  `<script src="/actor-control-panel.js" ${MARKER}></script>`
+].join('\n');
 
-if (!express.response.__asActorControlPatched) {
-  const nativeSend = express.response.send;
+function injectControlBootstrap(html) {
+  if (html.includes(MARKER)) return html;
+  if (html.includes('</head>')) return html.replace('</head>', `${LAN_CONTROL_BOOTSTRAP}\n</head>`);
+  return `${LAN_CONTROL_BOOTSTRAP}\n${html}`;
+}
 
-  express.response.send = function actorControlHtmlSend(body) {
-    if (
-      typeof body === 'string' &&
-      !body.includes(MARKER) &&
-      body.includes('</head>') &&
-      (this.req?.method === 'GET' || this.req?.method === 'HEAD')
-    ) {
-      let pathname = '';
-      try {
-        pathname = new URL(this.req.url, 'http://localhost').pathname;
-      } catch { /* ignore malformed request URL */ }
+if (!express.__asActorControlFactoryPatched) {
+  const currentExpressFactory = express;
 
-      if (pathname === '/' || pathname === '/index.html') {
-        body = body.replace('</head>', `${SCRIPT}\n</head>`);
-      }
-    }
+  function actorControlExpressFactory(...args) {
+    const app = currentExpressFactory(...args);
 
-    return nativeSend.call(this, body);
-  };
+    const serveControlPage = (req, res, next) => {
+      fs.readFile(CONTROL_PATH, 'utf8', (error, html) => {
+        if (error) return next();
+        res.type('html');
+        res.setHeader('Cache-Control', 'no-store');
+        if (req.method === 'HEAD') return res.end();
+        return res.send(injectControlBootstrap(html));
+      });
+    };
 
-  express.response.__asActorControlPatched = true;
+    app.get('/', serveControlPage);
+    app.get('/index.html', serveControlPage);
+    return app;
+  }
+
+  Object.assign(actorControlExpressFactory, currentExpressFactory);
+  actorControlExpressFactory.application = currentExpressFactory.application;
+  actorControlExpressFactory.request = currentExpressFactory.request;
+  actorControlExpressFactory.response = currentExpressFactory.response;
+  actorControlExpressFactory.__asActorControlFactoryPatched = true;
+  require.cache[require.resolve('express')].exports = actorControlExpressFactory;
 }
